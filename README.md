56e9ab08b0060d1a9d70306202df3266d96e5ee6
Thita Charoentham <thita.charoentham35@qmail.cuny.edu>
2018-04-29 14:20:19 -0400

    display JSON string on textview

--- README.md
+++ README.md
@@ -26,3 +26,4 @@ display in a sensible way. Implement the JSON parsing in JsonUtils so it
 produces a Sandwich Object that can be used to populate the UI that you designed.
 # Sandwish-Club
 # Sandwish-Club
+# Sandwish-Club
--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,6 +3,7 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,20 +12,24 @@ import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
-    // create Sandwich obj
     private Sandwich sandwich;
-    public TextView tv_also_know;
+    public TextView tv_description, tv_also_know, tv_place_of_origin, tv_ingredients;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
-        tv_also_know = (TextView) findViewById(R.id.also_known_tv);
+        tv_place_of_origin = findViewById(R.id.place_of_origin_tv);
+        tv_description = findViewById(R.id.description_tv);
+        tv_also_know = findViewById(R.id.detail_also_known_tv);
+        tv_ingredients = findViewById(R.id.ingredients_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -40,7 +45,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -63,5 +68,31 @@ public class DetailActivity extends AppCompatActivity {
     private void populateUI() {
 
 
+        if (sandwich.getAlsoKnownAs().isEmpty()){
+            tv_also_know.setText("N/A");
+        }else{
+            tv_also_know.setText(printList(sandwich.getAlsoKnownAs()));
+        }
+        if(sandwich.getPlaceOfOrigin().isEmpty()){
+            tv_place_of_origin.setText("N/A");
+        }else{
+            tv_place_of_origin.setText(sandwich.getPlaceOfOrigin());
+        }
+        tv_description.setText(sandwich.getDescription());
+
+        if (sandwich.getIngredients().isEmpty()){
+            tv_ingredients.setText("N/A");
+        }else {
+            tv_ingredients.setText(printList(sandwich.getIngredients()));
+        }
+
+    }
+
+    private String printList(List<String> alsoKnownAs) {
+        String str = new String ();
+            for(int i =0 ; i < alsoKnownAs.size(); i++){
+                str += alsoKnownAs.get(i).toString() + " ";
+            }
+        return str;
     }
 }
